7c9607d73414eef7043a5d80c11f8c109a469f78
Jitendra Bhati <142474374+Jeetukgpian@users.noreply.github.com>
2024-07-30 11:48:47 +0000

    basic Variables

---
+++ 01_basics/variable.js
@@ -0,0 +1,16 @@
+const instiId="22BT10013"; //Store constant values
+let Name="Jitendra"; //Modern js - prefered
+var roomNo=141; //Not prefered due to block or functional scope
+city="Greater Noida";  // not prefered 
+let girlFriend; // value is not assigned => Undefined
+
+// instiId="22CH10013"; //Can't assign to const variable
+//Can change the values of other variable (var,let)
+
+console.log(instiId);
+//Modify above varibales
+Name="Jeetu Bhati";
+roomNo="142";
+city="Noida";
+console.table([instiId,Name,roomNo,city,girlFriend]);
+/* Console.table([val1,val2,val3]) is used to print multiple variable */
