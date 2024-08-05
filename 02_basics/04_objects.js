29ea0332b53852791fcdc514e9e3d50cb2e9ec58
Jitendra Bhati <142474374+Jeetukgpian@users.noreply.github.com>
2024-08-05 18:10:11 +0000

    Objects revised

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,85 @@
+//Other ways to create an object
+// let newObj=new Object();
+let obj1={}
+
+//assign key:value pairs from outside
+obj1.name="Jitendra"
+obj1.rollNo="22BT10013"
+obj1.section=8
+obj1.college="IIT KGP"
+
+console.log(obj1)
+
+//we can create nested objects
+let myObject={
+    newObject:{
+        otherObject:{
+            name:"Jeetu Bhai",
+            RollNo:19
+        }
+    }
+}
+
+//how to access these values
+console.log(myObject.newObject.otherObject.name)
+//to access keys of an object
+console.log(Object.keys(myObject))
+//to access values of an object
+console.log(Object.values(myObject))
+console.log(Object.entries(myObject)) //make every key:value pair an array
+
+let obj2={1:'a',2:'b'}
+let obj3={3:'c',4:'d'}
+let obj4={obj2,obj3}
+let obj5={...obj1,...obj2}
+let obj6=Object.assign({},obj1,obj2)
+
+console.log(obj2)
+console.log(obj3)
+console.log(obj4)
+console.log(obj5)
+console.log(obj6)
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+console.log(users[1].email) 
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+//other way to assign new variable to a given variable/key courseInstructor is replaced by instructor 
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+//other two ways to make an object => inside a way to create in js
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
