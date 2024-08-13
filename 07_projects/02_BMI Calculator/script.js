212b17b0dc8966c1d0cbcf91a997a658a02b8fad
Jitendra Bhati <142474374+Jeetukgpian@users.noreply.github.com>
2024-08-13 16:34:45 +0000

    updated

---
+++ 07_projects/02_BMI Calculator/script.js
@@ -0,0 +1,12 @@
+const form=document.querySelector('form')
+
+// console.log(form)
+
+form.addEventListener('submit',function (e){
+    // console.log(e)
+    // console.log(e.target)
+    const Height=document.querySelector('#height')
+    const Weight=document.querySelector('#weight')
+    const result=document.querySelector('#result')
+    
+})
